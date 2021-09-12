297771966083b57c1645f59bd8c13d9c751cda68
Aadhinana <ajibaa@gmail.com>
2021-09-12 22:59:11 +0530

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Basic Sample Hardhat Project
+
+Had to change the localnet chain Id from the metamask wallet to 31337 to match the hardhat's chainId.
+
+1. `npm i` Get dependencies
+2. `npx hardhat compile` Compile the contract into artifacts (abi)
+3. `npx hardhat node` Get the node running
+4. `npx hardhat run scripts/deploy.js --network localhost` Deploy to the localnode
+5. `npm run dev` Get frontend up.
+
+
+Built using [Hardhat](https://hardhat.org/) and [Vite](https://vitejs.dev/)
---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react'
+
+import { ethers } from "ethers";
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json';
+
+const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
+import './App.css'
+
+function App() {
+
+  const [input, setInpupt] = useState('');
+
+  async function fetchGreeting() {
+    if (typeof window.ethereum !== 'undefined') {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider);
+      try {
+        const data = await contract.greet();
+        console.log("The contract returned ", data);
+        setInpupt(data);
+      }
+      catch (e) {
+        console.log(e);
+      }
+    }
+  }
+
+  async function setGreeting() {
+    if (!input) return;
+    if (typeof window.ethereum !== 'undefined') {
+      await requestAccount();
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const chainId = await signer.getChainId();
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer);
+      try {
+        const tx = await contract.setGreeting(input);
+        await tx.wait();
+      } catch (error) {
+        console.log(error, error.message)
+      }
+    }
+  }
+
+  async function requestAccount() {
+    await window.ethereum.request({ method: 'eth_requestAccounts' });
+  }
+
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <button onClick={requestAccount}>Connect to wallet</button>
+      <button onClick={fetchGreeting}>Fetch Greeting</button>
+      <div>{input} is what the contract returned!</div>
+      <input type="text" placeholder="Enter greeting" onChange={e => setInpupt(e.target.value)} />
+      <button onClick={setGreeting}>Set Greeting</button>
+    </div>
+  )
+}
+
+export default App
